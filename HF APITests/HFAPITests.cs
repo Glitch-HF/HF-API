f87e4c97fd686989115590abc3cff9f3c61aac74
Glitch-HF <906348+Glitch-HF@users.noreply.github.com>
2020-07-19 09:48:35 -0400

    add testing project

---
+++ HF APITests/HFAPITests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HF_API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HF_API.Tests
+{
+    [TestClass()]
+    public class HFAPITests
+    {
+        [TestMethod()]
+        public void TryGetAuthTokenTest()
+        {
+        }
+    }
+}
